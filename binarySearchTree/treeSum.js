aa77c944512fc209a3f038b600d28ddb9d9adb86
Ameya Katgeri <ameyakatgeri@gmail.com>
2024-06-24 13:10:14 +0530

    implement treeSum problem (both bf and df approach)

---
+++ binarySearchTree/treeSum.js
@@ -0,0 +1,37 @@
+// breadth approach
+
+const treeSumBFS = (root) => {
+    if (root === null) {
+        return 0 
+    }
+
+    const queue = [ root ]
+    let sum = 0
+
+    while (queue.length > 0) {
+        const node = queue.shift()
+        sum += node.key
+
+        if (node.left !== null) {
+            queue.push(node.left)
+        }
+
+        if (node.right !== null) {
+            queue.push(node.right)
+        }
+    }
+
+    return sum
+
+}
+
+
+// depth approach
+
+const treeSumDFS = (root) => {
+    if (root === null) {
+        return 0
+    }
+
+    return root.key + treeSumDFS(root.left) + treeSumDFS(root.right)
+}
