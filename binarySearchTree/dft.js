79b03926845a85dced80dfb3ad83cf6ff3a7f359
Ameya Katgeri <ameyakatgeri@gmail.com>
2024-06-24 13:31:46 +0530

    implement iterative dft

--- binarySearchTree/dft.js
+++ binarySearchTree/dft.js
@@ -6,6 +6,7 @@
 //     }
 // }
 
+// recursive approach for recursive depth first traversal
 let recursiveDepthFirstTraversal = (root) => {
     if (root === null) {
         return []       // return an empty array/message
@@ -21,4 +22,31 @@ let recursiveDepthFirstTraversal = (root) => {
     // another way of returning the values
     // return [root.key, ...leftValues, ...rightValues]
     // output of above line : [a, b, c, d, e, f, g]
+}
+
+
+// iterative approach for depth first traversal
+let iterativeDepthFirstTraversal = (root) => {
+    if (root === null) {
+        return []// return an empty array or a message
+    }
+
+    const values = []
+    const stack = [ root ]
+
+    while (stack.length > 0) {
+        const node = stack.pop()
+        values.push(node.key)
+
+        if (node.right !== null) {
+            stack.push(node.right)
+        }
+
+        if (node.left !== null) {
+            stack.push(node.left)
+        }
+    }
+
+    return values
+
 }
