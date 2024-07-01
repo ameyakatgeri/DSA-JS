974357fe087651fd2a75bea46fa1bd4fec93261b
Ameya Katgeri <ameyakatgeri@gmail.com>
2024-07-01 13:04:41 +0530

    implement both iterative and recursive DFT on graph

---
+++ graph/dft.js
@@ -0,0 +1,38 @@
+const graph = {
+    a: ['b', 'c'],
+    b: ['a', 'c', 'd'],
+    c: ['a', 'b', 'd'],
+    d: ['b', 'c'],
+    e: ['f'],
+    f: ['e'],
+}
+
+
+// iterative approach of traversing the graph in DFT manner
+function iterativeDFT(graph, start) {
+    const stack = [ start ]
+    const visited = new Set()
+
+    while (stack.length > 0) {
+        const node = stack.pop()
+        if (!visited.has(node)) {
+            visited.add(node)
+            for (const neighbour of graph[a]) {
+                if (!visited.has(neighbour)) {
+                    stack.push(neighbour)
+                }
+            }
+        }
+    }
+}
+
+
+// recursive approach of traversing the graph in DFT manner
+function recursiveDFT(graph, start, visited = new Set()) {
+    if (!visited.has(start)) {
+        visited.add(start)
+        for (const neighbour of graph[a]) {
+            recursiveDFT(graph, neighbour, visited)
+        }
+    }
+}
