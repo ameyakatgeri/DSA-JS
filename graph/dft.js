391887141e912e10a8ff73199adba6bb3409febd
Ameya Katgeri <ameyakatgeri@gmail.com>
2024-07-01 20:35:23 +0530

    fix bugs(starting point of graph in both ways)

--- graph/dft.js
+++ graph/dft.js
@@ -17,7 +17,7 @@ function iterativeDFT(graph, start) {
         const node = stack.pop()
         if (!visited.has(node)) {
             visited.add(node)
-            for (const neighbour of graph[a]) {
+            for (const neighbour of graph[node]) {
                 if (!visited.has(neighbour)) {
                     stack.push(neighbour)
                 }
@@ -31,7 +31,7 @@ function iterativeDFT(graph, start) {
 function recursiveDFT(graph, start, visited = new Set()) {
     if (!visited.has(start)) {
         visited.add(start)
-        for (const neighbour of graph[a]) {
+        for (const neighbour of graph[start]) {
             recursiveDFT(graph, neighbour, visited)
         }
     }
